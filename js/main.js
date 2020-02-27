447155a90db8080e6e8498df53f84f9a3b85b36b
Jasmin-Mediani <molihua1992@gmail.com>
2020-02-27 14:59:21 +0100

    toggle... o show + hide

--- js/main.js
+++ js/main.js
@@ -5,3 +5,22 @@ $(".fa-bars").click(function () {
 $(".fa-times").click(function () {
     $(".hamburger-menu").toggle(500);
 });
+
+
+/*
+
+NOTA: al posto del primo .toggle potrei usare .show e al posto del secondo toggle potrei usare .hide
+
+
+
+$(".fa-bars").click(function () {
+    $(".hamburger-menu").show(500);
+});
+
+$(".fa-times").click(function () {
+    $(".hamburger-menu").hide(500);
+});
+
+
+*/
+
