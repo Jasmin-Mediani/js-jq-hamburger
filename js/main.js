3e14100484ab5150a4947917ddeb3577f05d89af
Jasmin-Mediani <molihua1992@gmail.com>
2020-02-27 14:45:54 +0100

    toggle!

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(".fa-bars").click(function () {
+    $(".hamburger-menu").toggle({ "display": "block" });
+});
+
+$(".fa-times").click(function () {
+    $(".hamburger-menu").toggle({ "display": "hidden" });
+});
